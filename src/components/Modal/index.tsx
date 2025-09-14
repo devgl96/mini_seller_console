0d9dd57a8dcbaed077111c1204806e3457c673b0
devgl96 <georgelucas.dev@gmail.com>
2025-09-14 00:35:51 -0300

    feat: update Modal component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,18 +1,12 @@
 import { useEffect, useRef } from "react";
 import { X } from "lucide-react";
-
-interface ModalProps {
-  children: React.ReactNode;
-  isOpen: boolean;
-  onClose: () => void;
-}
+import type { ModalProps } from "./types";
 
 export function Modal(props: ModalProps) {
-  const { children, isOpen, onClose } = props;
+  const { children, isOpen, onClose, title } = props;
 
   const modalRef = useRef<HTMLDivElement | null>(null);
 
-  // Close modal on outside click
   useEffect(() => {
     const handleClickOutside = (
       event: MouseEvent | React.MouseEvent<HTMLDivElement>
@@ -37,16 +31,30 @@ export function Modal(props: ModalProps) {
   if (!isOpen) return null;
 
   return (
-    <div
-      className="absolute top-6 right-20 translate-x-1/2 bg-white flex z-50 border border-gray-600 rounded-lg w-auto h-auto p-2"
-      ref={modalRef}
-    >
-      <X
+    <>
+      <div
+        className={`absolute inset-0 bg-black transition-opacity duration-300 ease-in-out z-40 ${
+          isOpen ? "opacity-50 visible" : "opacity-0 invisible"
+        }`}
         onClick={onClose}
-        size="16"
-        className="cursor-pointer top-1 right-1 absolute hover:text-gray-400"
-      />
-      <div className="p-4 rounded-lg">{children}</div>
-    </div>
+      ></div>
+      <div
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white z-50 border border-gray-600 rounded-lg p-2"
+        ref={modalRef}
+      >
+        <div className="px-6 py-4 border-b border-gray-200 flex items-center justify-between">
+          <div>
+            <h2 className="text-lg font-semibold text-gray-900">{title}</h2>
+          </div>
+          <button
+            onClick={onClose}
+            className="p-2 hover:bg-gray-100 rounded-full"
+          >
+            <X className="w-5 h-5 text-gray-400" />
+          </button>
+        </div>
+        <div className="p-4 rounded-lg">{children}</div>
+      </div>
+    </>
   );
 }
